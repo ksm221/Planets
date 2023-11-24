c4ce145dc8516a05f2f408bddb7b2e6ea9447cb1
Opilane TTHK <opilane@tthk.ee>
2023-11-24 12:38:56 +0200

    planets

---
+++ MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace Planets;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Montsserat-Medium.ttf", "RegularFont");
+				fonts.AddFont("Montsserat-SemiBold.ttf", "MediumFont");
+                fonts.AddFont("Montsserat-Bold.ttf", "BoldFont");
+            });
+
+		return builder.Build();
+	}
+}
---
+++ Views/PlanetsPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace Planets.Views;
+
+public partial class PlanetsPage : ContentPage
+{
+	public PlanetsPage()
+	{
+		InitializeComponent();
+	}
+
+	async void ApiPic_Clicked(System.Object sender, System.EventArgs e)
+	{
+
+	}
+
+    async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+    {
+
+    }
+}
